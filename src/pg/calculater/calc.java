76902762c67b1ecf4393c6eb08f4c09fb01d28ad
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-09-15 23:06:18 +0200

    Adding while loop to continue operator

--- src/pg/calculater/calc.java
+++ src/pg/calculater/calc.java
@@ -3,50 +3,67 @@ package pg.calculater;
 import java.util.Scanner;
 
 public class Calc {
-	public static void main(String[] args) {
 
+	static boolean contains(char c, char[] array) {
+		for (char x : array) {
+			if (x == c) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
 
 		char operator;
 		Double n1, n2, result;
-		@SuppressWarnings("resource")
+		char[] storOper = { '+', '-', '*', '/' };
 
-		Scanner input = new Scanner(System.in);
-		System.out.println("Choose an Operator: +, -, *, / or Press any key to exit....");
-		operator = input.next().charAt(0);
+		while (true) {
+			@SuppressWarnings("resource")
+			Scanner input = new Scanner(System.in);
+			System.out.println("Choose an Operator: +, -, *, / or Press any key to exit....");
+			operator = input.next().charAt(0);
 
-		System.out.println("Enter The First Number");
-		n1 = input.nextDouble();
+			if (contains(operator, storOper) == true) {
+				System.out.println("Enter The First Number");
+				n1 = input.nextDouble();
 
-		System.out.println("Enter second number");
-		n2 = input.nextDouble();
+				System.out.println("Enter second number");
+				n2 = input.nextDouble();
 
-		switch (operator) {
-		case '+':
-			result = n1 + n2;
-			System.out.println(n1 + " + " + n2 + " = " + result);
-			break;
-		case '-':
-			result = n1 - n2;
-			System.out.println(n1 + " - " + n2 + " = " + result);
-			break;
-		case '*':
-			result = n1 * n2;
-			System.out.println(n1 + " * " + n2 + " = " + result);
-			break;
-		case '/':
-			if (n2 == 0) {
-				System.out.println("Cannot divide by zero");
+				switch (operator) {
+				case '+':
+					result = n1 + n2;
+					System.out.println(n1 + " + " + n2 + " = " + result);
+					break;
+				case '-':
+					result = n1 - n2;
+					System.out.println(n1 + " - " + n2 + " = " + result);
+					break;
+				case '*':
+					result = n1 * n2;
+					System.out.println(n1 + " * " + n2 + " = " + result);
+					break;
+				case '/':
+					if (n2 == 0) {
+						System.out.println("Cannot divide by zero");
+					} else {
+						result = n1 / n2;
+						System.out.println(n1 + " / " + n2 + " = " + result);
+					}
+					break;
+
+				default:
+					System.out.println("Invalid operator!");
+					break;
+				}
 			} else {
-				result = n1 / n2;
-				System.out.println(n1 + " / " + n2 + " = " + result);
+				System.out.println("The system has been terminated");
+				System.exit(0);
+				break;
 			}
-			break;
-
-		default:
-			System.out.println("Invalid operator!");
-			break;
 		}
-
 	}
 
 }
