35b41f7413b9d3846e6b0ffb89474fd57890f955
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-09-15 23:20:29 +0200

    Add comment in for all statement in calc.java

--- src/pg/calculater/Calc.java
+++ src/pg/calculater/Calc.java
@@ -3,7 +3,7 @@ package pg.calculater;
 import java.util.Scanner;
 
 public class Calc {
-
+	// Create default method for searching in Array return (True or False) 
 	static boolean contains(char c, char[] array) {
 		for (char x : array) {
 			if (x == c) {
@@ -14,18 +14,21 @@ public class Calc {
 	}
 
 	public static void main(String[] args) {
-
+		// Create Variable for Calculator
 		char operator;
 		Double n1, n2, result;
 		char[] storOper = { '+', '-', '*', '/' };
-
+		// Access to the calculator system
 		while (true) {
+			// create an object of Scanner on class
 			@SuppressWarnings("resource")
 			Scanner input = new Scanner(System.in);
+			// ask users to enter the operator
 			System.out.println("Choose an Operator: +, -, *, / or Press any key to exit....");
 			operator = input.next().charAt(0);
 
 			if (contains(operator, storOper) == true) {
+				// ask users to enter the numbers
 				System.out.println("Enter The First Number");
 				n1 = input.nextDouble();
 
@@ -33,18 +36,22 @@ public class Calc {
 				n2 = input.nextDouble();
 
 				switch (operator) {
+				// Calculator addition process
 				case '+':
 					result = n1 + n2;
 					System.out.println(n1 + " + " + n2 + " = " + result);
 					break;
+				// Calculator subtraction process
 				case '-':
 					result = n1 - n2;
 					System.out.println(n1 + " - " + n2 + " = " + result);
 					break;
+				// Multiplication in calculator
 				case '*':
 					result = n1 * n2;
 					System.out.println(n1 + " * " + n2 + " = " + result);
 					break;
+				// division process in calculator
 				case '/':
 					if (n2 == 0) {
 						System.out.println("Cannot divide by zero");
@@ -59,6 +66,7 @@ public class Calc {
 					break;
 				}
 			} else {
+				// Terminated for Calculater
 				System.out.println("The system has been terminated");
 				System.exit(0);
 				break;
